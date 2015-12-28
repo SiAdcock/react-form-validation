caa83edada11b1ff60d595d8fbe0f3d422ed4fd1
Simon Adcock <simonadcock@notonthehighstreet.com>
2015-12-28 17:13:37 +0000

    User info form

--- app/components/main.js
+++ app/components/main.js
@@ -4,7 +4,27 @@ const React = require('react');
 class Main extends React.Component {
   render() {
     return (
-      <div>Hello</div>
+      <form>
+        <div className="form-row">
+          <label>First Name:</label>
+          <input type="text"/>
+        </div>
+        <div className="form-row">
+          <label>Last Name:</label>
+          <input type="text"/>
+        </div>
+        <div className="form-row">
+          <label>Email:</label>
+          <input type="email"/>
+        </div>
+        <div className="form-row">
+          <label>Password:</label>
+          <input type="password"/>
+        </div>
+        <div className="form-row">
+          <button type="button">Submit</button>
+        </div>
+      </form>
     );
   }
 }
